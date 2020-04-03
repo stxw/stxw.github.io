8a62f205be367d943f322b60968e74db61ba088d
nchuxw <1601325@stu.nchu.edu.cn>
2020-04-03 17:52:43 +0800

    update theme live2d

--- themes/pure/assets/media/live2d/main.js
+++ themes/pure/assets/media/live2d/main.js
@@ -14,17 +14,19 @@ function addElement(elementName, src) {
 }
 var tempDiv = document.createElement('div');
 tempDiv.innerHTML = `
-            <div class="waifu" style="bottom:-95px">
-		        <div class="waifu-tips"></div>
-		        <div style="overflow:hidden;"><canvas id="live2d" width='280' height="500" style="top:-120px" class="live2d"></canvas></div>
-		        <div class="waifu-tool">
-		            <span class="fui-chat"></span>
-		            <span class="fui-user"></span>
-		            <span class="fui-photo"></span>
-		            <span class="fui-home"></span>
-		        </div>
-            </div>
-  `;
+<div class="waifu">
+	<div class="waifu-tips"></div>
+	<div style="overflow:hidden;">
+		<canvas id="live2d" class="live2d" width="300" height="370" ></canvas>
+	</div>
+	<div class="waifu-tool">
+		<span class="fui-chat"></span>
+		<!--<span class="fui-user"></span>-->
+		<span class="fui-photo"></span>
+		<span class="fui-home"></span>
+	</div>
+</div>
+`;
 document.body.appendChild(tempDiv.children[0]);
 addElement('link', 'media/live2d/waifu.css');
 addElement('script', 'media/live2d/live2d.js')
