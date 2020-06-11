e9b714b136d7ade7c0d3bfa5c8f830fd4ce5a7bc
nchuxw <16201325@stu.nchu.edu.cn>
2020-06-12 01:30:59 +0800

    update load to main

--- themes/sagiri/source/js/nchuxw/load_to_main.js
+++ themes/sagiri/source/js/nchuxw/load_to_main.js
@@ -1,9 +1,11 @@
 window.onload = function () {
 	var pathname = window.location.pathname;
-	if(pathname != "/") {
-		// document.getElementById("main").scrollIntoView();
+	var pre_pathname = sessionStorage.getItem("pre_pathname");
+	if(pathname != "/" && pathname != pre_pathname)
+	{
 		$('html, body').animate({
 			scrollTop: $("#main").offset().top - 50
-        }, 500);
+		}, 500);
 	}
+	sessionStorage.setItem("pre_pathname", pathname);
 }
