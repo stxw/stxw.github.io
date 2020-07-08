97d7aa4756346bf1a5e4b884ac46c06b1134e2ce
nchuxw <1601325@stu.nchu.edu.cn>
2020-07-08 11:58:55 +0800

    update main.js

--- themes/sagiri/source/js/main.js
+++ themes/sagiri/source/js/main.js
@@ -2,7 +2,8 @@ const stxw = {
 	load_to_main: function(){
 		var pathname = window.location.pathname;
 		var pre_pathname = sessionStorage.getItem("pre_pathname");
-		if(pathname != "/" && pathname != pre_pathname)
+		var cur_st = $(document).scrollTop();
+		if(pathname != "/" && cur_st < 10 && pathname != pre_pathname)
 		{
 			$('html, body').animate({
 				scrollTop: $("#main").offset().top - 50
